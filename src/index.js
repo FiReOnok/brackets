2a2d9730c15be143dca6e6da3c42bbd4b17632e0
Yakov Kungurtsev <fireonok@gmail.com>
2021-03-16 00:41:35 +0400

    Only 60% of tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let openCloseRatio = 0;
+
+    for (let bracketsIndex = 0; bracketsIndex < bracketsConfig.length; bracketsIndex++){
+        for (let i = 0; i < str.length; i++){
+            if (str[i] === bracketsConfig[bracketsIndex][0]) {
+                openCloseRatio++;
+            } else if (str[i] === bracketsConfig[bracketsIndex][1]){
+                openCloseRatio--;
+                if (openCloseRatio < 0 ) {
+                    return false;
+                }
+            }
+        }
+    }
+    return openCloseRatio === 0;
 }
